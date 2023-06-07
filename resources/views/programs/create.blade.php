4152d59e363104362deedb60b2e3129218264328
wa1ead <waliddraa2@gmail.com>
2023-06-07 14:04:22 +0100

    CUSTOMIZATION

--- resources/views/programs/create.blade.php
+++ resources/views/programs/create.blade.php
@@ -2,8 +2,27 @@
 @section('content')
 
 <div class="content">
-  <h2>{{ $program['program'] }}</h2>
-  <p>{{ $program['training'] }}</p>
+
+  <div class="flex justify-center">
+    <h2>Create a new Program</h2>
+  </div>
+
+  <div class="flex justify-center">
+    <form action="" method="post" class="form bg-white p-6 border-1">
+      <div>
+        <label for="program-name">Program</label>
+        <input id="program-name" name="program-name" type="text" >
+      </div>
+      <div>
+        <label for="program-training">Training</label>
+        <input id="program-training" name="program-training" type="text" >
+      </div>
+      <div>
+        <button type="submit">Save</button>
+      </div>
+    </form>
+  </div>
+
 </div>
 </body>
 
